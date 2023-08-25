791818a2b2d8ad22cd20f1f8bbc6432364d5c0cf
rebwar <rebwar1.azizi@gmail.com>
2023-08-25 14:51:57 +0100

    Add AWS Configuration Component and Styling Updates

---
+++ frontend/src/components/generic/textTranslationPolly/Credentials.js
@@ -0,0 +1,9 @@
+// AWSSettings.js
+import AWS from "aws-sdk";
+
+// AWS.config.update({
+//   region: "your-region",
+//   credentials: new AWS.Credentials("your-access-key", "your-secret-key"),
+// });
+
+export default AWS;
