4e34346bd50bc5979918cce6e7b011d8ed2a35d9
rebwar <rebwar1.azizi@gmail.com>
2023-08-31 14:34:54 +0100

    install database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express, { json } from "express";
+import mysql from "mysql2";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -7,6 +8,16 @@ import multer, { memoryStorage } from "multer";
 import { getUserPresignedUrls, uploadToS3 } from "./s3.js";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "azad4582",
+  database: "DriverCheckInDB",
+  authPlugins: {
+    mysql_clear_password: () => () => Buffer.from("azad4582"),
+  },
+});
 
 const PORT = process.env.PORT || 4000;
 
@@ -22,9 +33,35 @@ app.use(
 );
 app.use(json());
 
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+app.get("/check-ins", (req, res) => {
+  const query = "SELECT * FROM CheckIns"; // Using the correct table name
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error("Error querying the database:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+    console.log("Check-ins retrieved from the database:", results);
+    return res.status(200).json(results);
+  });
+});
+
 //TODO Serve static files from the React app
-// const _dirname = path.dirname("");
-// const buildPath = path.join(_dirname, "../frontend/build");
+/**
+ * ! const _dirname = path.dirname("");
+ *! const buildPath = path.join(_dirname, "../frontend/build");
+ *! app.use(express.static(buildPath));
+ */
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const buildPath = path.join(__dirname, "../frontend/build");
+
 // app.use(express.static(buildPath));
 
 // app.get("/*", function (req, res) {
@@ -37,44 +74,6 @@ app.use(json());
 //     }
 //   );
 // });
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const buildPath = path.join(__dirname, "../frontend/build");
-
-app.use(express.static(buildPath));
-
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-/**
- *!using .mjs files instead of commonJS
-  const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const buildPath = path.join(__dirname, "../frontend/build");
-
-app.use(express.static(buildPath));
-
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-*!using .mjs files instead of commonJS
- */
 //TODO end of Serve static files from the React app
 
 app.post("/images", upload.single("image"), (req, res) => {
