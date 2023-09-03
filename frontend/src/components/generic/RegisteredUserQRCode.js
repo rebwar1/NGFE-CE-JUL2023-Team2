769cc7a6309a6d08bfc13ccaf66a8204328f901e
rebwar <rebwar1.azizi@gmail.com>
2023-09-03 18:26:08 +0100

    Integrate form submission and QR code generation

--- frontend/src/components/generic/RegisteredUserQRCode.js
+++ frontend/src/components/generic/RegisteredUserQRCode.js
@@ -1,3 +1,265 @@
+// import React, { useState, useRef } from "react";
+// import { Form, Input, Button, message } from "antd";
+// import QRCode from "qrcode.react";
+// import Webcam from "react-webcam";
+// import { Row, Col, Space, Spin } from "antd";
+// import { Image, Text } from "@chakra-ui/react";
+// import useMutation from "../../hooks/useMutation";
+// import useQuery from "../../hooks/useQuery";
+// import { Card } from "antd";
+// import { PrinterOutlined } from "@ant-design/icons";
+// import ReactToPrint from "react-to-print";
+
+// import "../../App.css";
+
+// const validFileTypes = ["image/jpg", "image/jpeg", "image/png"];
+// const URL = "/images";
+
+// const ErrorText = ({ children, ...props }) => (
+//   <Text fontSize="lg" color="red.300" {...props}>
+//     {children}
+//   </Text>
+// );
+
+// const Posts = () => {
+//   const [userData, setUserData] = useState({});
+//   const [qrData, setQRData] = useState("");
+//   const [lastSubmittedImage, setLastSubmittedImage] = useState(null);
+//   const [showCard, setShowCard] = useState(false); // Add showCard state variable
+
+//   const handleFormSubmit = async values => {
+//     const currentDate = new Date();
+//     const formattedDate = currentDate
+//       .toISOString()
+//       .slice(0, 19)
+//       .replace("T", " ");
+
+//     const updatedValues = {
+//       ...values,
+//       timestamp: formattedDate,
+//     };
+
+//     setUserData(updatedValues);
+//     setQRData(JSON.stringify(updatedValues));
+
+//     try {
+//       const response = await fetch("http://localhost:4000/save-check-in", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(updatedValues),
+//       });
+
+//       if (response.ok) {
+//         console.log("Check-in saved successfully!");
+//         message.success("Check-in saved successfully!");
+
+//         // Handle QR code generation here or call a separate function
+//       } else {
+//         console.error("Error: Unable to save check-in.");
+//         message.error("Error: Unable to save check-in. Please try again.");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       message.error("Error: Unable to save check-in. Please try again later.");
+//     }
+//   };
+
+//   const [refetch, setRefetch] = useState(0);
+//   const webcamRef = useRef(null);
+//   const [imageUrls, setImageUrls] = useState([]);
+
+//   const {
+//     mutate: uploadImage,
+//     isLoading: uploading,
+//     error: uploadError,
+//   } = useMutation({ url: URL });
+
+//   const {
+//     data: fetchedImageUrls = [],
+//     isLoading: imagesLoading,
+//     error: fetchError,
+//   } = useQuery(URL, refetch);
+
+//   const [error, setError] = useState("");
+
+//   const handleUpload = async () => {
+//     const imageData = webcamRef.current.getScreenshot();
+//     const blob = await fetch(imageData).then(r => r.blob());
+//     const fileType = blob.type;
+
+//     if (!validFileTypes.includes(fileType)) {
+//       setError("Invalid image format. Please use jpg, jpeg, or png.");
+//       return;
+//     }
+
+//     const form = new FormData();
+//     form.append("image", blob, "selfie." + fileType.split("/")[1]);
+
+//     try {
+//       // Upload the image and set showCard to true when the upload is successful
+//       await uploadImage(form);
+//       setShowCard(true); // Show the card
+//       setImageUrls(prevUrls => [imageData, ...prevUrls]);
+//       setLastSubmittedImage(imageData);
+//       setError("");
+//     } catch (error) {
+//       setError("Error uploading image. Please try again.");
+//     }
+//   };
+
+//   const cardRef = useRef(null);
+
+//   return (
+//     <Row gutter={16} align="middle">
+//       <Col xs={24} md={12}>
+//         <p className="normal-text margin-text">Driver check-in</p>
+//         <Form onFinish={handleFormSubmit}>
+//           <Form.Item label="Name" name="name" required>
+//             <Input />
+//           </Form.Item>
+//           <Form.Item label="Family Name" name="familyName" required>
+//             <Input />
+//           </Form.Item>
+//           <Form.Item label="Email" name="email" required>
+//             <Input />
+//           </Form.Item>
+//           <Form.Item
+//             label="Vehicle Registration Number"
+//             name="vehicleNumber"
+//             required
+//           >
+//             <Input />
+//           </Form.Item>
+//           <Form.Item label="Company Name" name="companyName" required>
+//             <Input />
+//           </Form.Item>
+
+//           <Form.Item>
+//             <Button type="primary" htmlType="submit" block>
+//               Generate QR Code
+//             </Button>
+//           </Form.Item>
+//         </Form>
+
+//         {/* {qrData && (
+//           <div>
+//             <QRCode value={qrData} />
+//             <p>Timestamp: {userData.timestamp}</p>
+//           </div>
+//         )} */}
+//       </Col>
+//       <Col xs={24} md={12}>
+//         <p className="normal-text margin-text">Take a selfie</p>
+//         <Webcam
+//           audio={false}
+//           ref={webcamRef}
+//           screenshotFormat="image/jpeg"
+//           className="webcam"
+//         />
+//         {error && <ErrorText>{error}</ErrorText>}
+//         <Space>
+//           <Button
+//             type="primary"
+//             className="capture"
+//             onClick={handleUpload}
+//             disabled={uploading} // Disable the button when uploading
+//           >
+//             Capture and Upload
+//           </Button>
+//           {uploading && <Spin />}
+//         </Space>
+//       </Col>
+//       <Col xs={24} md={24}>
+//         {imagesLoading && <Spin />}
+//         {fetchError && <ErrorText>{fetchError.message}</ErrorText>}
+//       </Col>
+//       <Col xs={24} md={24}>
+//         {showCard && userData && (
+//           <>
+//             <Card
+//               title={`${userData.name || ""} ${userData.familyName || ""}`}
+//               ref={cardRef}
+//               style={{ width: 400 }}
+//               cover={
+//                 <Image
+//                   src={lastSubmittedImage || "/images/default-selfie.jpg"}
+//                   alt="Driver"
+//                 />
+//               }
+//             >
+//               <div>
+//                 <strong>Name:</strong> {userData.name || ""}
+//               </div>
+//               <div>
+//                 <strong>Family Name:</strong> {userData.familyName || ""}
+//               </div>
+//               <div>
+//                 <strong>Email:</strong> {userData.email || ""}
+//               </div>
+//               <div>
+//                 <strong>Vehicle Number:</strong>{" "}
+//                 {userData.vehicleNumber || "N/A"}
+//               </div>
+//               <div>
+//                 <strong>Company Name:</strong> {userData.companyName || "N/A"}
+//               </div>
+//               <div>
+//                 <strong>Timestamp:</strong> {userData.timestamp || ""}
+//               </div>
+//               <div style={{ textAlign: "center", marginTop: "1rem" }}>
+//                 {qrData && (
+//                   <div>
+//                     <QRCode
+//                       style={{
+//                         margin: "auto",
+//                       }}
+//                       value={qrData}
+//                     />
+//                     <div
+//                       style={{
+//                         display: "flex",
+//                         alignItems: "center",
+//                         justifyContent: "center", // Center the NGF Logo horizontally
+//                         marginTop: "1rem", // Add some margin between the QRCode and the logo
+//                       }}
+//                     >
+//                       <img
+//                         src="https://ngfimg.s3.eu-west-2.amazonaws.com/NGF-logo.jpg"
+//                         alt="NGF Logo"
+//                         style={{
+//                           width: "200px",
+//                           height: "auto",
+//                         }}
+//                       />
+//                     </div>
+//                   </div>
+//                 )}
+//               </div>
+//             </Card>
+
+//             <ReactToPrint
+//               trigger={() => (
+//                 <Button type="primary" icon={<PrinterOutlined />} block>
+//                   Print
+//                 </Button>
+//               )}
+//               content={() => cardRef.current}
+//             />
+//           </>
+//         )}
+//       </Col>
+//       <Col xs={24}></Col>
+//     </Row>
+//   );
+// };
+
+// export default Posts;
+
+//🍉🍉🍉🍉🍉🍉🍉
+//🍉🍉🍉🍉🍉🍉🍉
+
 import React, { useState, useRef } from "react";
 import { Form, Input, Button, message } from "antd";
 import QRCode from "qrcode.react";
@@ -25,7 +287,9 @@ const Posts = () => {
   const [userData, setUserData] = useState({});
   const [qrData, setQRData] = useState("");
   const [lastSubmittedImage, setLastSubmittedImage] = useState(null);
-  const [showCard, setShowCard] = useState(false); // Add showCard state variable
+  const [showForm, setShowForm] = useState(true);
+  const [showSelfieCapture, setShowSelfieCapture] = useState(false);
+  const [showCard, setShowCard] = useState(false);
 
   const handleFormSubmit = async values => {
     const currentDate = new Date();
@@ -41,6 +305,30 @@ const Posts = () => {
 
     setUserData(updatedValues);
     setQRData(JSON.stringify(updatedValues));
+
+    try {
+      const response = await fetch("http://localhost:4000/save-check-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedValues),
+      });
+
+      if (response.ok) {
+        console.log("Check-in saved successfully!");
+        message.success("Check-in saved successfully!");
+        setShowForm(false); // Hide the form
+        setShowSelfieCapture(true); // Show the selfie capture section
+        // Handle QR code generation here or call a separate function
+      } else {
+        console.error("Error: Unable to save check-in.");
+        message.error("Error: Unable to save check-in. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      message.error("Error: Unable to save check-in. Please try again later.");
+    }
   };
 
   const [refetch, setRefetch] = useState(0);
@@ -86,105 +374,68 @@ const Posts = () => {
     }
   };
 
-  const [lastSubmittedData, setLastSubmittedData] = useState(null);
-
-  const handleQRFormSubmit = async () => {
-    const formData = {
-      ...userData,
-      timestamp: new Date().toISOString().slice(0, 19).replace("T", " "),
-    };
-
-    try {
-      const response = await fetch("http://localhost:4000/save-check-in", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        console.log("Check-in saved successfully!");
-        message.success("Check-in saved successfully!");
-        setLastSubmittedData(formData);
-      } else {
-        console.error("Error: Unable to save check-in.");
-        message.error("Error: Unable to save check-in.");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      message.error("Error: Unable to save check-in. Please try again later.");
-    }
-  };
-
   const cardRef = useRef(null);
 
   return (
     <Row gutter={16} align="middle">
       <Col xs={24} md={12}>
-        <p className="normal-text margin-text">Driver check-in</p>
-        <Form onFinish={handleFormSubmit}>
-          <Form.Item label="Name" name="name" required>
-            <Input />
-          </Form.Item>
-          <Form.Item label="Family Name" name="familyName" required>
-            <Input />
-          </Form.Item>
-          <Form.Item label="Email" name="email" required>
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Vehicle Registration Number"
-            name="vehicleNumber"
-            required
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item label="Company Name" name="companyName" required>
-            <Input />
-          </Form.Item>
+        {showForm && (
+          <>
+            <p className="normal-text margin-text">Driver check-in</p>
+            <Form onFinish={handleFormSubmit}>
+              <Form.Item label="Name" name="name" required>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Family Name" name="familyName" required>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Email" name="email" required>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Vehicle Registration Number"
+                name="vehicleNumber"
+                required
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item label="Company Name" name="companyName" required>
+                <Input />
+              </Form.Item>
 
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              block
-              onClick={handleQRFormSubmit}
-            >
-              Generate QR Code
-            </Button>
-          </Form.Item>
-        </Form>
-
-        {/* {qrData && (
-          <div>
-            <QRCode value={qrData} />
-            <p>Timestamp: {userData.timestamp}</p>
-          </div>
-        )} */}
+              <Form.Item>
+                <Button type="primary" htmlType="submit" block>
+                  Generate QR Code
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        )}
+        {showSelfieCapture && !showCard && (
+          <>
+            <p className="normal-text margin-text">Take a selfie</p>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              className="webcam"
+            />
+            {error && <ErrorText>{error}</ErrorText>}
+            <Space>
+              <Button
+                type="primary"
+                className="capture"
+                onClick={handleUpload}
+                disabled={uploading}
+              >
+                Capture and Upload
+              </Button>
+              {uploading && <Spin />}
+            </Space>
+          </>
+        )}
       </Col>
       <Col xs={24} md={12}>
-        <p className="normal-text margin-text">Take a selfie</p>
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          className="webcam"
-        />
-        {error && <ErrorText>{error}</ErrorText>}
-        <Space>
-          <Button
-            type="primary"
-            className="capture"
-            onClick={handleUpload}
-            disabled={uploading} // Disable the button when uploading
-          >
-            Capture and Upload
-          </Button>
-          {uploading && <Spin />}
-        </Space>
-      </Col>
-      <Col xs={24} md={24}>
         {imagesLoading && <Spin />}
         {fetchError && <ErrorText>{fetchError.message}</ErrorText>}
       </Col>
@@ -269,5 +520,3 @@ const Posts = () => {
 };
 
 export default Posts;
-
-//🍉
--- frontend/src/components/users/ReportAccident.js
+++ frontend/src/components/users/ReportAccident.js
@@ -1,5 +1,28 @@
-import React from "react";
+// import React from "react";
 
-export default function ReportAccident() {
-  return <h1>report</h1>;
+// export default function ReportAccident() {
+//   return <h1>report</h1>;
+// }
+
+//🍋
+import React from "react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import All from "./All";
+import "../../assets/scss/dashboard.scss";
+
+export default function DashboardSignup() {
+  const user = {
+    name: "John Doe",
+    email: "john@example.com",
+    // Other user details
+  };
+  return (
+    <div className="dashboard">
+      <ChakraProvider>
+        <Box textAlign="center" fontSize="2xl" m="3rem auto" p={5} maxW={700}>
+          <All user={user} />
+        </Box>
+      </ChakraProvider>
+    </div>
+  );
 }
