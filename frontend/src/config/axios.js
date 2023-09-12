0ce5954d60b571da722497a043ef1c0eb9bcf5f9
rebwar <rebwar1.azizi@gmail.com>
2023-09-13 00:44:43 +0100

    use window insted .git

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,17 +1,38 @@
+// import axios from "axios";
+
+// const baseURL = process.env.REACT_APP_API_URL;
+// const USER_ID = 123;
+
+// const axiosClient = axios.create({
+//   baseURL,
+//   headers: {
+//     "x-user-id": USER_ID,
+//   },
+// });
+
+// export const axiosClientWithoutHeader = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL,
+// });
+
+// export default axiosClient;
+//! get it from html file
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL;
+// Get the apiAddress from the window object
+const apiAddress = window.apiAddress;
+// || "http://localhost:8181"; // Default value
+
 const USER_ID = 123;
 
 const axiosClient = axios.create({
-  baseURL,
+  baseURL: apiAddress, // Use the apiAddress obtained from the window object or the default value
   headers: {
     "x-user-id": USER_ID,
   },
 });
 
 export const axiosClientWithoutHeader = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: apiAddress,
 });
 
 export default axiosClient;
