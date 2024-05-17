e9d183b2b815b0ba2a5e83da46881f9effd5647d
rebwar <rebwar1.azizi@gmail.com>
2024-05-17 14:41:16 +0100

    create new branch

--- frontend/src/components/generic/textTranslationPolly/Credentials.js
+++ frontend/src/components/generic/textTranslationPolly/Credentials.js
@@ -4,16 +4,25 @@ import AWS from "aws-sdk";
 //   region: "your-region",
 //   credentials: new AWS.Credentials("your-access-key", "your-secret-key"),
 // });
+// ⭐️
 
-AWS.config.update({
-  region: "eu-west-2",
-  credentials: new AWS.Credentials(
-    "AKIA4MTWLMO2GNEKG35R",
-    "jhx/83Id39wPsFaYlwFWYTmdFJSL/wjfIQZK1O0k"
-  ),
-});
+// ⭐️
+
 // AWS.config.update({
 //   region: "eu-west-2",
 // });
 
 export default AWS;
+
+// import AWS from "aws-sdk";
+
+// // The environment variable AWS_SDK_LOAD_CONFIG is set to 1 to ensure config is loaded from the config file.
+// process.env.AWS_SDK_LOAD_CONFIG = "1";
+
+// // Configure the AWS SDK with the desired region.
+// AWS.config.update({
+//   region: "eu-west-2",
+// });
+
+// // Now you can use the AWS SDK with credentials from the .aws/credentials file
+// export default AWS;
